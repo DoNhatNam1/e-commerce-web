a65b900383e745e4aae1bbfa73ef0b24bbd478cc
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-08-27 19:25:39 +0700

    Initial commit

---
+++ apps/product-step-and-auth/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="product-step-and-auth" />
+    </div>
+  );
+}
+
+export default App;
